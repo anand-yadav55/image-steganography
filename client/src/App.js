f856ab0742cf8adccabdd6730ef2331f6e4272bd
anand-yadav55 <anand55yadav@gmail.com>
2021-11-17 11:24:19 +0530

    drop image setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
+import fileIcon from './assets/file-icon.jpg';
 
 function App() {
   return (
     <div className="App">
       <div className="main">
         <div className="dropArea">
-          <div className="placeholderUploadImage">DROP YOUR IMAGE HERE.</div>
-          <input className="uploadButton" type="file" accept="image/*" />
+          <div className="upload">
+            <form>
+              <div className="fileIcon">
+                <img src={fileIcon} alt="" />
+              </div>
+              <div className="placeholderUploadImage">
+                DROP YOUR IMAGE HERE.
+              </div>
+              <div className="upload">
+                <input type="button" className="uploadButton" value="Browse" />
+                <input
+                  type="file"
+                  name="upload"
+                  accept="image/*"
+                  id="fileUpload"
+                />
+                <span className="fileName">Select file..</span>
+              </div>
+              <input
+                type="button"
+                className="uploadButton"
+                value="Upload File"
+              />
+            </form>
+          </div>
         </div>
       </div>
     </div>
